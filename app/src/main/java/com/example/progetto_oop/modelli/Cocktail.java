53f7e13a104fc3a5c72892bc4ee122668c820276
lucamiano <lucatriooo@gmail.com>
2022-08-14 19:48:56 +0200

    change description

--- app/src/main/java/com/example/progetto_oop/modelli/Cocktail.java
+++ app/src/main/java/com/example/progetto_oop/modelli/Cocktail.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Cocktail implements Serializable, Comparable<Cocktail> {
     /**
-     * id del drink da bere
+     * id del drink da bere ciao
      */
 
     public final int id;
